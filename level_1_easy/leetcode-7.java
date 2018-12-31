ba962e4b7f87f28b47c9f1e27ddbab8ce780fda1
ChihI Chung(鍾志怡) <chihi_chung@asus.com>
2018-12-31 17:00:58 +0800

    Create folders

---
+++ level_1_easy/leetcode-7.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * Given a 32-bit signed integer, reverse digits of an integer.
+     * 
+     * Example 1:
+     * Input: 123
+     * Output: 321
+     * 
+     * Example 2:
+     * Input: -123
+     * Output: -321
+     * 
+     * Example 3:
+     * Input: 120
+     * Output: 21
+     */
+    public static void main(String[] args) {
+        
+    }
+    public int reverse(int x) {
+        
+    }
+}
